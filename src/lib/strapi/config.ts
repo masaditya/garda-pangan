0e2953355cc5f1e2b98717f58d3094e09c1f709c
Naufaldi <naufaldi.rafif@gmail.com>
2026-08-25 22:29:32 +0700

    fix(frontend): use production Strapi URL

--- src/lib/strapi/config.ts
+++ src/lib/strapi/config.ts
@@ -1,12 +1,12 @@
-const DEFAULT_STRAPI_URL = 'http://43.156.236.252:1337'
+const DEFAULT_STRAPI_URL = 'https://garda.naufaldi.com'
 
 export function getStrapiBaseUrl() {
-  const env = import.meta.env ?? {}
-  const configuredUrl = env.STRAPI_URL || env.VITE_STRAPI_URL
+  const env = import.meta.env
+  const configuredUrl = env.PUBLIC_STRAPI_URL || env.STRAPI_URL || env.VITE_STRAPI_URL
 
   return (configuredUrl || DEFAULT_STRAPI_URL).replace(/\/$/, '')
 }
 
 export function getStrapiApiToken() {
-  return import.meta.env?.STRAPI_API_TOKEN
+  return import.meta.env.STRAPI_API_TOKEN
 }
